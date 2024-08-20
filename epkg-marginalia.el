8461f744737c66345af9b345b2efe87c7553b510
Jonas Bernoulli <jonas@bernoul.li>
2024-08-20 02:09:18 +0200

    Improve setup instructions

--- epkg-marginalia.el
+++ epkg-marginalia.el
@@ -38,8 +38,8 @@
 ;; provided by that as well.
 
 ;;   (with-eval-after-load 'marginalia
-;;     (cl-pushnew 'epkg-marginalia-annotate-package
-;;                 (alist-get 'package marginalia-annotator-registry)))
+;;     (setcar (alist-get 'package marginalia-annotator-registry)
+;;             #'epkg-marginalia-annotate-package))
 
 ;;; Code:
 
